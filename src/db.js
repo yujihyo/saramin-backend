ba129edcb69f6c914fed995a6e2cb460d5cfecb0
yujihyo <rgh822@naver.com>
2024-12-14 21:34:01 +0900

    db

--- src/db.js
+++ src/db.js
@@ -1,1 +1,20 @@
-{\rtf1}
+const mysql = require('mysql2');
+
+// MySQL 연결 설정
+const db = mysql.createConnection({
+  host: 'localhost',    // MySQL 서버 주소 (로컬에서 실행 중이면 localhost)
+  user: 'root',         // MySQL 사용자명
+  password: 'Rgh1226!',  // MySQL 비밀번호
+  database: 'saramin_db', // 사용할 데이터베이스 이름
+});
+
+// MySQL 연결 확인
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결 실패:', err.message);
+    return;
+  }
+  console.log('MySQL에 연결되었습니다.');
+});
+
+module.exports = db;
